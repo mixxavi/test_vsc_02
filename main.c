94b16beda6f05e97dcbe109e81e85cdc9ed5f8ae
xavi moreno <xavi-moreno@catxmp.es>
2021-09-19 07:45:28 +0200

    Added two new lines after 'hello world' printf

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main (void)
 {
-    printf("Hello world"); // This comment has created by a multicursor tool. Hello again.
+    printf("Hello world\n\n"); // This comment has created by a multicursor tool. Hello again.
     printf("hello"); // This comment has created by a multicursor tool.
     return 0; // This comment has created by a multicursor tool. Hello again.
 }
